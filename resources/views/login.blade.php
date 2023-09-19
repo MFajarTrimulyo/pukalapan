06a6f6e29acccd45af9cd5022a21396a5ca3a4ff
Athalaa <mrathalaa@gmail.com>
2023-09-19 07:26:49 +0700

    login page

---
+++ resources/views/login.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login Pukalapan</title>
+  <link rel="stylesheet" href="{{ asset('css/publik/.css') }}">
+
+</head>
+<body>
+  
+  <div className="LOG-IN-PAGE">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="background">
+            <div className="overlap-group">
+              <img
+                className="background-layer"
+                alt="Background"
+                src="https://c.animaapp.com/SNdUwhTY/img/background-layer2.png"
+              />
+              <img
+                className="registerpage-button"
+                alt="Registerpage button"
+                src="https://c.animaapp.com/SNdUwhTY/img/registerpage-button@2x.png"
+              />
+              <img
+                className="loginpage-button"
+                alt="Loginpage button"
+                src="https://c.animaapp.com/SNdUwhTY/img/loginpage-button@2x.png"
+              />
+              <div className="picture" />
+              <div className="text-wrapper">Designed by Katemangostar</div>
+            </div>
+          </div>
+          <div className="input-user">
+            <div className="div">Forget password?</div>
+            <div className="text-wrapper-2">Don’t have an account?</div>
+            <div className="text-wrapper-3">Sign up</div>
+            <div className="text-wrapper-4">or login with :</div>
+            <button className="input-button">
+              <div className="div-wrapper">
+                <div className="text-wrapper-5">LOGIN</div>
+              </div>
+            </button>
+            <div className="passwordlogin-input">
+              <div className="input">
+                <MdiPassword className="mdi-password" />
+              </div>
+              <div className="password">
+                <div className="text-wrapper-6">Password</div>
+              </div>
+            </div>
+            <div className="usernamelogin-input">
+              <div className="input">
+                <img className="mdi-user" alt="Mdi user" src="https://c.animaapp.com/SNdUwhTY/img/mdi-user.svg" />
+              </div>
+              <div className="username">
+                <div className="text-wrapper-6">Email/Username</div>
+              </div>
+            </div>
+          </div>
+          <div className="LOGIN-HERE">LOG IN</div>
+          <div className="text-wrapper-7">UKALAPAN</div>
+          <div className="text-wrapper-8">P</div>
+          <img
+            className="icon-facebook"
+            alt="Icon facebook"
+            src="https://c.animaapp.com/SNdUwhTY/img/---icon--facebook-@2x.png"
+          />
+          <img
+            className="icon-google"
+            alt="Icon google"
+            src="https://c.animaapp.com/SNdUwhTY/img/---icon--google-1-@2x.png"
+          />
+        </div>
+      </div>
+    </div>
+</body>
+</html>
+    
+ 
