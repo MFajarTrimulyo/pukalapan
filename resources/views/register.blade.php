58c5c516a00dfff3adc31aa69a669eb7187483c8
Athalaa <mrathalaa@gmail.com>
2023-09-19 09:59:10 +0700

    register

---
+++ resources/views/register.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <link rel="stylesheet" href="{{  asset('css/register.css')  }}">
+  </head>
+  <body>
+    <div class="REGISTER-PAGE">
+      <div class="overlap-wrapper">
+        <div class="overlap">
+          <div class="background">
+            <div class="overlap-group">
+              <img class="background-layer" src="img/background-layer2.png" />
+              <img class="registerpage-button" src="img/registerpage-button.png" />
+              <img class="loginpage-button" src="img/loginpage-button.png" />
+            </div>
+          </div>
+          <div class="input-user">
+            <button class="input-button">
+              <div class="div-wrapper"><div class="div">REGISTER</div></div>
+            </button>
+            <div class="text-wrapper-2">Already have an account?</div>
+            <div class="text-wrapper-3">Log In</div>
+            <div class="div-2">
+              <div class="password"><div class="text-wrapper-4">Confirm Password</div></div>
+              <div class="input"><img class="mdi-password" src="img/mdi-password.svg" /></div>
+            </div>
+            <div class="passwordregister">
+              <div class="password-2"><div class="text-wrapper-4">Password</div></div>
+              <div class="input"><img class="mdi-password" src="img/mdi-password.svg" /></div>
+            </div>
+            <div class="emailregister-input">
+              <div class="password-3"><div class="text-wrapper-4">Email</div></div>
+              <div class="input"><img class="dashicons-email" src="img/dashicons-email.svg" /></div>
+            </div>
+            <div class="usernameregister">
+              <div class="username"><div class="text-wrapper-4">Username</div></div>
+              <div class="input"><img class="mdi-user" src="img/mdi-user.svg" /></div>
+            </div>
+          </div>
+          <div class="text-wrapper-5">REGISTER</div>
+          <div class="text-wrapper-6">UKALAPAN</div>
+          <div class="text-wrapper-7">P</div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
