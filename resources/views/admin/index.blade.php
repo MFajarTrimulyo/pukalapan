3075a0ce5c5716769a5ff21ddc4405848080e286
M Fajar Tri <90810685+MFajarTrimulyo@users.noreply.github.com>
2023-09-19 11:16:26 +0700

    admin page

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,18 +1,16 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Admin Page</title>
-    @vite('input.css')
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="css/output.css">
 </head>
 <body>
-    @section()
-    
-    @yield('isi')
-    
-    @endsection
-    
+    <h1 class="text-3xl font-mono">
+        Hello world!
+    </h1>
 </body>
 </html>
