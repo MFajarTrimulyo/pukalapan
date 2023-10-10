5284655b88f1657c946d42cce057c5ebc546d209
Farhan-L4h <muhanmedan06@gmail.com>
2023-10-10 07:58:05 +0700

    Update web.php
    HomePage

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', function () {
+    return view('homepage.index');
+});
 
 Route::get('/kategori', function () { return view('page.kategori'); });
 Route::get('/isi', function () {return view('homepage.isi');});
